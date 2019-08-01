5fede05bab790f045d54116970eb61b7e9af2c74
Abhijit Roy <abhijit@scl.gov.in>
2019-08-01 17:31:30 +0530

    Bot: Dog CEO API added

--- bot.py
+++ bot.py
@@ -3,27 +3,30 @@ from input import *
 import requests
 import json
 from bs4 import BeautifulSoup
-
+ 
 
 bot = botogram.create(API_key)
 bot.about = "This is a Dog Bot.\nThis gives random images of Dog."
 bot.owner = "@abhi3700"
 
 
+"""
+This functions reads the stream.
+"""
 def readstream(r):
     rsvp_list = []
     for raw_rsvp in r.iter_lines():
         if raw_rsvp:
-            rsvp = json.loads(raw_rsvp)
-            rsvp_list.append(rsvp)
+            rsvp_list.append(json.loads(raw_rsvp))    
     return rsvp_list
+
 
 @bot.command("show")
 def show_command(chat, message, args):
     """Show random images of Dogs"""
-    response = requests.get("https://random.dog/woof.json?filter=mp4,webm", verify=False)
+    response = requests.get("https://dog.ceo/api/breed/labrador/images/random", verify=False)
     html_page = readstream(response)
-    chat.send(html_page[0]['url'])
+    chat.send(html_page[0]['message'])
     # chat.send("https://random.dog/woof.json?filter=mp4,webm")
     
 if __name__ == "__main__":
